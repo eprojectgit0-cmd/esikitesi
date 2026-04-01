8e820e91b05d58f4e9e5c71c14c4d8fb88ab7345
eprojectgit0-cmd <eprojectgit0@gmail.com>
2026-04-01 23:09:41 +0500

    Delete app..js

--- app..js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    document.getElementById('sideMenu').classList.toggle('open');
-    document.getElementById('overlay').classList.toggle('active');
-}
-
-function closeMenu() {
-    document.getElementById('sideMenu').classList.remove('open');
-    document.getElementById('overlay').classList.remove('active');
-}
